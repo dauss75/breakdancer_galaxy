561f4dafe03f928f7736fbff09004be332a41c3d
dauss75 <sjung@Seguns-MacBook-Pro.local>
2016-07-06 04:19:33 -0500

    remove files

--- GDGraph-1.52/samples/error.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use GD::Graph::Data;
-
-my $data = GD::Graph::Data->new()    
-      or die GD::Graph::Data->error;
-  $data->read(file => '/foo/bar.data') 
-        or die $data->error;
-
